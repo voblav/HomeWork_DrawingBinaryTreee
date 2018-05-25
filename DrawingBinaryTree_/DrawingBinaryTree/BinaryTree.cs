feb278db7d9402c0a6ca19ee330bd43d216dfe2c
voblav <35613203+voblav@users.noreply.github.com>
2018-05-25 19:58:05 +0300

    Add files via upload

---
+++ DrawingBinaryTree_/DrawingBinaryTree/BinaryTree.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingBinaryTree
+{
+    public class BinaryTreeItem<T>
+    {
+        public T Value;
+        public BinaryTreeItem<T> parent;
+        public BinaryTreeItem<T> left;
+        public BinaryTreeItem<T> right;
+        public BinaryTreeItem(T Value, BinaryTreeItem<T> parent = null, BinaryTreeItem<T> left = null, BinaryTreeItem<T> right = null)
+        {
+            this.Value = Value;
+            this.parent = parent;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class BinaryTree<T> where T : IComparable
+    {
+
+        private BinaryTreeItem<T> _root = null;
+        private bool allowDuplicateKeys = false;
+        private int _count = 0;
+        public int Count { get => _count; }
+        public BinaryTreeItem<T> RootItem
+        {
+            get
+            {
+                return _root;
+            }
+        }
+
+        private void Add(T value, BinaryTreeItem<T> item)
+        {
+            if (!allowDuplicateKeys && value.CompareTo(item.Value) == 0)
+            {
+                item.Value = value;
+            }
+            else if (value.CompareTo(item.Value) < 0)
+            {
+                //go to left
+                if (item.left == null)
+                {
+                    item.left = new BinaryTreeItem<T>(value, item);
+                    ++_count;
+                }
+                else
+                {
+                    Add(value, item.left);
+                }
+            }
+            else //go to right
+            {
+                if (item.right == null)
+                {
+                    item.right = new BinaryTreeItem<T>(value, item);
+                    ++_count;
+                }
+                else
+                {
+                    Add(value, item.right);
+                }
+            }
+        }
+
+        private IEnumerator<BinaryTreeItem<T>> GetItemEnumerator(BinaryTreeItem<T> item)
+        {
+            Stack<BinaryTreeItem<T>> stackItem = new Stack<BinaryTreeItem<T>>();
+            while (item != null || stackItem.Count != 0)
+            {
+                if (stackItem.Count != 0)
+                {
+                    item = stackItem.Pop();
+                    yield return item;
+
+                    if (item.right != null) item = item.right;
+                    else item = null;
+                }
+                while (item != null)
+                {
+                    stackItem.Push(item);
+                    item = item.left;
+                }
+            }
+        }
+
+        private BinaryTreeItem<T> GetItemFromValue(T value, BinaryTreeItem<T> item)
+        {
+            if (item.Value.Equals(value))
+                return item;
+            else if (item.Value.CompareTo(value) > 0)
+            {
+                if (item.left != null)
+                    return GetItemFromValue(value, item.left);
+            }
+            else
+            {
+                if (item.right != null)
+                    return GetItemFromValue(value, item.right);
+            }
+            return null;
+        }
+
+        private void RemoveItem(BinaryTreeItem<T> item, BinaryTreeItem<T> parent)
+        {
+            if (item.left == null && item.right == null)
+            {
+                RemoveItemWithoutChildren(item, parent);
+            }
+            else if (item.left != null && item.right != null)
+            {
+                RemoveItemWithBothChildren(item, parent);
+            }
+            else
+            {
+                RemoveItemWithOneChild(item, parent);
+            }
+            --_count;
+        }
+
+        private void RemoveItemWithoutChildren(BinaryTreeItem<T> item, BinaryTreeItem<T> parent)
+        {
+            if (item == _root)
+            {
+                _root = null;
+                return;
+            }
+            if (parent.left == item)
+            {
+                parent.left = null;
+            }
+            else
+            {
+                parent.right = null;
+            }
+        }
+
+        private void RemoveItemWithOneChild(BinaryTreeItem<T> item, BinaryTreeItem<T> parent)
+        {
+            BinaryTreeItem<T> childItem;
+
+            childItem = item.left ?? item.right;
+
+            if (parent.left == item)
+            {
+                parent.left = childItem;
+            }
+            else
+            {
+                parent.right = childItem;
+            }
+
+            childItem.parent = parent;
+        }
+
+        private void RemoveItemWithBothChildren(BinaryTreeItem<T> item, BinaryTreeItem<T> parent)
+        {
+            //Find successor Node
+            BinaryTreeItem<T> success = item.right;
+            BinaryTreeItem<T> successParent = item;
+            bool tmp = true;
+            while (success.left != null)
+            {
+                tmp = false;
+                successParent = success;
+                success = success.left;
+            }
+            if (tmp)
+            {
+                item.Value = item.right.Value;
+                item.right = item.right.right;
+                return;
+            }
+            item.Value = success.Value;
+            successParent.left = success.right;
+
+            if (success.right != null) success.right.parent = successParent;
+        }
+
+        public void Add(T value)
+        {
+            if (_root == null)
+            {
+                _root = new BinaryTreeItem<T>(value);
+                ++_count;
+            }
+            else Add(value, _root);
+        }
+
+        public void Remove(T value)
+        {
+            var item = GetItemFromValue(value, RootItem);
+            if (item != null)
+            {
+                RemoveItem(item, item.parent);
+            }
+                
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            using (IEnumerator<BinaryTreeItem<T>> e = GetItemEnumerator(_root))
+            {
+                while (e.MoveNext())
+                {
+                    yield return e.Current.Value;
+                }
+            }
+        }
+    }
+}
---
+++ DrawingBinaryTree_/DrawingBinaryTree/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DrawingBinaryTree
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtItem = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtItem
+            // 
+            this.TxtItem.Location = new System.Drawing.Point(560, 30);
+            this.TxtItem.Name = "TxtItem";
+            this.TxtItem.Size = new System.Drawing.Size(100, 20);
+            this.TxtItem.TabIndex = 0;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(570, 60);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 1;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.AddItem_Click);
+            // 
+            // BtnDel
+            // 
+            this.BtnDel.Location = new System.Drawing.Point(570, 89);
+            this.BtnDel.Name = "BtnDel";
+            this.BtnDel.Size = new System.Drawing.Size(75, 23);
+            this.BtnDel.TabIndex = 2;
+            this.BtnDel.Text = "Delete";
+            this.BtnDel.UseVisualStyleBackColor = true;
+            this.BtnDel.Click += new System.EventHandler(this.RemoveItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 321);
+            this.Controls.Add(this.BtnDel);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.TxtItem);
+            this.Name = "MainForm";
+            this.Text = "Drawing a Binary Tree";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtItem;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnDel;
+    }
+}
+
